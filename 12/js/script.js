8819f03f41368d0fa618d814eb7171a2aa4e3e3c
skvorzof <skvorzof@ya.ru>
2021-04-11 14:02:52 +0300

    v2

--- 12/js/script.js
+++ 12/js/script.js
@@ -1,4 +1,6 @@
 const gol = $('#gol');
+const csk = $('#csk');
+const zenit = $('#zenit');
 
 const ball = $('.ball');
 ball.on('click', move);
@@ -11,8 +13,21 @@ const width = $('.field').width();
 
 const gate = height / 2; // Место для ворот
 
+//ссылки на счётчик
+const one = count();
+const two = count();
+
 function rand() {
   return Math.floor(Math.random() * (height - ballHeight));
+}
+
+// счётчик голов
+function count() {
+  let cnt = 0;
+
+  return function () {
+    return (cnt += 1);
+  };
 }
 
 function move() {
@@ -24,13 +39,24 @@ function move() {
   if (left > width / 2) hit = 0;
 
   // Анимация мяча x, y
-  ball.animate({
-    left: hit,
-    top: randHeight,
-  });
+  ball.animate(
+    {
+      left: hit,
+      top: randHeight,
+    },
+    {
+      duration: 800,
+      step: function (now) {
+        $(this).css({ transform: 'rotate(' + now * 5 + 'deg)' });
+      },
+    }
+  );
 
   // Размер ворот - 2 мяча
   if (randHeight > gate - ballHeight && randHeight < gate + ballHeight) {
+    // Счёт голов
+    left > 0 ? csk.text(one) : zenit.text(two);
+
     gol.text('Ура гол 🥳').fadeIn(500).delay(1000).fadeOut(500); // Анимация забитого гола
   }
 }
